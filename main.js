30528f74d2e71651dd38608999e7be73fec555b2
ket-id <ket-id@outlook.com>
2024-04-17 17:40:39 -0300

    Finaliza Javascript

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ function playSonido(idElementoAudio) {
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < 9) {
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     // evita la repeticion de codigo
     const tecla = listaDeTeclas[contador]
     // trae la segunda clase del item
@@ -16,10 +16,37 @@ while (contador < 9) {
     tecla.onclick = function () {
         playSonido(idAudio);
     };
-    // suma uno a cada vuelta del while
-    contador = contador + 1;
-    console.log('Vuelta ' + contador);
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('activa');
+        }
+
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('activa');
+    }
 }
+
+
+////////////////////////////////////////////// misma funcion pero arriba mejor optimizado
+// let contador = 0;
+
+// while (contador < 9) {
+//     // evita la repeticion de codigo
+//     const tecla = listaDeTeclas[contador]
+//     // trae la segunda clase del item
+//     const instrumento = tecla.classList[1]
+//     // template string
+//     const idAudio = `#sonido_${instrumento}`;
+
+//     tecla.onclick = function () {
+//         playSonido(idAudio);
+//     };
+//     // suma uno a cada vuelta del while
+//     contador = contador + 1;
+//     console.log('Vuelta ' + contador);
+// }
 
 
 
